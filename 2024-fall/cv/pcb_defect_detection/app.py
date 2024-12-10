b951d5a989ed79bf1d470ac81e0e2383a19b36ee
devilhyt <devilhyt@gmail.com>
2024-12-10 14:54:04 +0800

    update cv final project

--- 2024-fall/cv/pcb_defect_detection/app.py
+++ 2024-fall/cv/pcb_defect_detection/app.py
@@ -2,6 +2,10 @@ import streamlit as st
 from streamlit_extras.metric_cards import style_metric_cards
 from ultralytics import YOLO
 from PIL import Image
+import pandas as pd
+
+# setting the app
+st.set_page_config(layout="wide")
 
 style_metric_cards(
     background_color="#262730",
@@ -10,31 +14,39 @@ style_metric_cards(
     box_shadow=False,
 )
 
-
+# load and cache the model
 @st.cache_resource
 def load_model():
     return YOLO("pcb_defect/v3/weights/best.pt")
 
-
+# a flag to check if the image is updated
 def file_uploader_on_change():
     st.session_state["image_updated"] = True
 
-
+# initilaize the app 
 if "image_updated" not in st.session_state:
     st.session_state["image_updated"] = True
-
 model = load_model()
 
+# main app
 st.title("PCB Defect Detection")
-st.image('./image/pcb.jpg', use_container_width=True)
 st.divider()
+# st.image('./image/pcb.jpg', use_container_width=True)
+col1, col2 = st.columns(2, gap="large")
 
-st.header("Choose an PCB image")
-uploaded_file = st.file_uploader("", type=["jpg", "jpeg", "png"], on_change=file_uploader_on_change)
-st.divider()
+# image uploader
+with col1:
+    st.header("Choose an PCB image")
+    uploaded_file = st.file_uploader(
+        "", type=["jpg", "jpeg", "png"], on_change=file_uploader_on_change
+    )
+    st.divider()
 
-# Predict only when image is uploaded.
-if uploaded_file is not None and st.session_state["image_updated"]:
+# predict the image only if the image is uploaded and updated
+image_updated = st.session_state["image_updated"]
+if uploaded_file is None:
+    st.session_state["label_count"] = pd.Series()
+elif image_updated:
     image = Image.open(uploaded_file)
     results = model.predict(image)
     annotated_frame = results[0].plot()
@@ -44,24 +56,30 @@ if uploaded_file is not None and st.session_state["image_updated"]:
     st.session_state["label_count"] = label_count
     st.session_state["image_updated"] = False
 
-# Display the results.
-if uploaded_file is not None:
-    st.subheader("Defects Count")
-    cols = st.columns(3)
+# Defects Count
+COLUMNS = 3
+with col1:
+    st.header("Defects Count")
+    cols = st.columns(COLUMNS)
     label_count = st.session_state["label_count"]
     for i, name in enumerate(model.names.values()):
-        with cols[i % 3]:
-            st.metric(label=name, value=label_count.get(name, 0))
-        if (i + 1) % 3 == 0 and i + 1 < len(model.names):
-            cols = st.columns(3)
+        with cols[i % COLUMNS]:
+            value = label_count.get(name, 0)
+            st.metric(label=name, value=value)
     st.divider()
 
-    st.subheader("Fault Points")
+# Fault Points
+IMAGE_WIDTH = 480
+with col2:
+    st.header("Fault Points")
     image_switch = st.checkbox("Show Original Image")
-    if image_switch:
-        image = st.session_state["image"]
-        st.image(image)
-    else:
-        annotated_frame = st.session_state["annotated_frame"]
-        st.image(annotated_frame)
-    st.divider()  
+
+    if uploaded_file is not None:
+        if image_switch:
+            image = st.session_state["image"]
+            st.image(image, width=IMAGE_WIDTH)
+        else:
+            annotated_frame = st.session_state["annotated_frame"]
+            st.image(annotated_frame, width=IMAGE_WIDTH)
+
+    st.divider()
